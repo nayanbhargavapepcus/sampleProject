48aca1d8f1823e1b501fa01f053e3331471ac97c
nayanbhargavapepcus <nayan.bhargava@pepcus.com>
2023-09-18 18:25:36 -0700

    Adding completed and active tabs to sort data

--- sampleTODO_reactTs/src/components/todos.tsx
+++ sampleTODO_reactTs/src/components/todos.tsx
@@ -1,9 +1,22 @@
 import { useTodos, Todo } from "../store/todos"
+import { useSearchParams } from "react-router-dom"
 
 const Todos = () => {
     const {todos, toggleTodoAsCompleted, handleDeleteTodo} = useTodos();
 
+    const [searchParams] = useSearchParams();
+    let todosData = searchParams.get("todos")
+    console.log("what are search params", todosData)
+
     let filterData = todos;
+
+    if (todosData === "active") {
+        filterData = filterData.filter((task) => !task.completed)
+    }
+
+    if (todosData === "completed") {
+        filterData = filterData.filter((task) => task.completed)
+    }
 
   return (
     <ul>
